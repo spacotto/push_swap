aabaa4b8ccef898e1666cb2729a3d2b7b7215d72
Silvia Pacotto <spacotto@c1r5p5.42lehavre.fr>
2026-01-08 18:08:41 +0100

    update push_swap

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,19 +6,22 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 16:46:26 by spacotto          #+#    #+#             */
-/*   Updated: 2026/01/08 17:51:41 by spacotto         ###   ########.fr       */
+/*   Updated: 2026/01/08 18:03:24 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	get_stack(int ac, char **av)
+t_stack	get_tokens(int ac, char **av)
 {
 	t_stack	stack_a;
 	int	i;
 
 	i = 1;
 	while (i <= ac + 1)
+	{
+		
+	}
 	return (stack_a);
 }
 
@@ -26,9 +29,13 @@ int	main(int ac, char **av)
 {
 	int	i;
 
+	i = 0;
 	if (ac > 1)
 	{
-		stack_a = get_stack(ac, av);
+		while (i <= ac)
+		{
+			i++;
+			stack_a = get_stack(ac, av);
 		push_swap(stack_a);
 	}
 	return (0);
