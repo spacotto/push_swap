97e0a1a1707a2795a858f8c44b9177a7123ffbb4
Silvia Pacotto <spacotto@c1r7p3.42lehavre.fr>
2026-01-14 15:36:51 +0100

    added search list to find node position

---
+++ libft/srcs/ft_lstchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/14 15:27:39 by spacotto          #+#    #+#             */
+/*   Updated: 2026/01/14 15:33:04 by spacotto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstchr(t_list *haystack, t_list *needle)
+{
+	t_list	current;
+	int		position;
+
+	position = 0;
+	current = haystack;
+	while (current != needle)
+	{
+		position++;
+		current = current->next;
+	}
+	return (position);
+}
