14320f4ff4d2c2eafc9e66382986abe50d27da5b
Silvia Pacotto <spacotto@c1r7p3.42lehavre.fr>
2026-01-15 17:12:34 +0100

    chunk sort update

--- srcs/chunk_sort.c
+++ srcs/chunk_sort.c
@@ -6,14 +6,29 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:46:50 by spacotto          #+#    #+#             */
-/*   Updated: 2026/01/14 18:02:45 by spacotto         ###   ########.fr       */
+/*   Updated: 2026/01/15 17:10:41 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static int	chunk_size(t_list *stack)
+{
+	int	stack_size;
+	int	chunk_size;
+
+	stack_size = ft_lstsize(stack);
+	if (stack_size <= 16)
+		chunk_size = 4;
+	else if (stack_size <= 100)
+		chunk_size = 20;
+	else
+		chunk_size = 45;
+	return (chunk_size);
+}
+
 void	chunk_sort(t_stacks *stacks)
 {
-	if (ft_lstsize(stacks->stack_a) == 2)
-		return ;
+	assign_index(stacks->stack_a);
+	chunk_size(stacks->stack_a);
 }
