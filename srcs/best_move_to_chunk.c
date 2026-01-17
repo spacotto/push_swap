11b252bdacb597859386e34bf11d88b21806894a
spacotto <silvia.pacotto@gmail.com>
2026-01-17 19:17:35 +0100

    Rename function from cheapest_in_chunk to cheapest_move_to_chunk

--- srcs/best_move_to_chunk.c
+++ srcs/best_move_to_chunk.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   best_in_chunk.c                                    :+:      :+:    :+:   */
+/*   best_move_to_chunk.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -11,3 +11,39 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+t_list *cheapest_move_to_chunk(t_list *stack, int min, int max)
+{
+	t_list	*best_node;
+	int		best_cost;
+	int		cost;
+	int		index;
+	int		size;
+
+	best_node = NULL;
+	best_cost = INT_MAX;
+	cost = 0;
+	index = 0;
+	size = ft_lstsize(stack);
+
+	while (stack)
+		{
+			if (stack->index >= min && stack->index <= max)
+				{
+					if (index <= size / 2)
+						cost = index; // ra
+					else
+						cost = size - index; // rra
+					if (cost < best_cost)
+						{
+							best_cost = cost;
+							best_node = stack;
+							if (cost <= 1) // at 0 and 1 we can't get better
+								return (best_node);
+						}
+				}
+			stack = stack->next;
+			index++;
+		}
+	return (best_node);
+}
