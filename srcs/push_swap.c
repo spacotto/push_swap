bac3e49df4a53306c2de93e6b7365225dd64e9c1
spacotto <spacotto@student.42lehavre.fr>
2026-01-18 19:02:05 +0100

    have to fix the optimisation

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 16:46:26 by spacotto          #+#    #+#             */
-/*   Updated: 2026/01/18 17:51:07 by spacotto         ###   ########.fr       */
+/*   Updated: 2026/01/18 18:00:45 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,9 +116,10 @@ int	main(int ac, char **av)
 			i++;
 		}
 		push_swap(&stacks);
-		optimise_ops(&stacks);
+		if (stacks.stack_a == NULL)
+			optimise_ops(&stacks);
 		print_ops(&stacks);
-		ft_lstclear(&stacks.operations, del);
+//		ft_lstclear(&stacks.operations, del);
 		ft_lstclear(&stacks.stack_a, del);
 	}
 	return (0);
