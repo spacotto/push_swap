9a78b1828e4e661ce629bd95bb2da71baf7c8b7c
spacotto <silvia.pacotto@gmail.com>
2026-01-19 11:26:47 +0100

    Update ops_remove.c

--- srcs/ops_remove.c
+++ srcs/ops_remove.c
@@ -12,28 +12,65 @@
 
 #include "push_swap.h"
 
-void	rm_ra_rra(t_list  *ops)
+static int	is_ra_rra(char *s1, char *s2)
 {
-	if ((ft_strcmp(cmp1->content, "ra\n") == 0 &&
-		ft_strcmp(cmp2->content, "rra\n") == 0) ||
-		(ft_strcmp(cmp1->content, "rra\n") == 0 &&
-		ft_strcmp(cmp2->content, "ra\n") == 0))
-		{
-			free(cmp1->content);
-			free(cmp2->content);
-			return ;
-		}
+	if (!s1 || !s2)
+		return (0);
+	if (ft_strcmp(s1, "ra\n") == 0 && ft_strcmp(s2, "rra\n") == 0)
+		return (1);
+	if (ft_strcmp(s1, "rra\n") == 0 && ft_strcmp(s2, "ra\n") == 0)
+		return (1);
+	return (0)
 }
 
-void	rm_rb_rrb(t_list  *ops)
+static int	is_rb_rrb(char *s1, char *s2)
 {
-	if ((ft_strcmp(cmp1->content, "ra\n") == 0 &&
-		ft_strcmp(cmp2->content, "rra\n") == 0) ||
-		(ft_strcmp(cmp1->content, "rra\n") == 0 &&
-		ft_strcmp(cmp2->content, "ra\n") == 0))
+	if (!s1 || !s2)
+		return (0);
+	if (ft_strcmp(s1, "rb\n") == 0 && ft_strcmp(s2, "rrb\n") == 0)
+		return (1);
+	if (ft_strcmp(s1, "rrb\n") == 0 && ft_strcmp(s2, "rb\n") == 0)
+		return (1);
+	return (0);
+}
+
+static void	delete_redundant_pair(t_list **ops, t_list *prev, t_list *cmp1)
+{
+	t_list	*cmp2;
+
+	cmp2 = cmp1->next;
+	if (prev)
+		prev->next = cmp2->next;
+	else
+		*ops = cmp2->next;
+	ft_lstdelone(cmp1, del);
+	ft_lstdelone(cmp2, del);
+}
+
+void	rm_redundancy(t_list **ops)
+{
+	t_list	*prev;
+	t_list	*cmp1;
+
+	if (!ops || !*ops || !(*ops)->next)
+		return ;
+	prev = NULL;
+	cmp1 = *ops;
+	while (cmp1 && cmp1->next)
+	{
+		if (is_ra_rra(cmp1->content, cmp1->next->content) ||
+			is_rb_rrb(cmp1->content, cmp1->next->content))
 		{
-			free(cmp1->content);
-			free(cmp2->content);
-			return ;
+			delete_redundant_pair(ops, prev, cmp1);
+			if (prev)
+				cmp1 = prev->next;
+			else
+				cmp1 = *ops;
 		}
+		else
+		{
+			prev = cmp1;
+			cmp1 = cmp1->next;
+		}
+	}
 }
