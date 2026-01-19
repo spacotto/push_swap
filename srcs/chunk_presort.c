c2d12a6279fb35bda3310184296b49582a257479
spacotto <spacotto@student.42lehavre.fr>
2026-01-19 23:50:55 +0100

    last 3 sort fixed

--- srcs/chunk_presort.c
+++ srcs/chunk_presort.c
@@ -6,7 +6,7 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 17:08:08 by spacotto          #+#    #+#             */
-/*   Updated: 2026/01/19 22:55:19 by spacotto         ###   ########.fr       */
+/*   Updated: 2026/01/19 23:48:50 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,51 +19,49 @@ static int	choose_chunk_size(int stack_size)
 	if (stack_size <= 100)
 		chunk_size = 33;
 	else
-		chunk_size = 60;
+		chunk_size = 66;
 	return (chunk_size);
 }
 
-static void	build_chunk(t_stacks *stacks, t_chunk *chunk, t_biggest *b)
+static void    build_chunk(t_stacks *stacks, t_chunk *chunk, t_biggest *b)
 {
-	t_list		*target;
-	int			mid;
+    t_list        *target;
+    int            mid;
 
-	mid = chunk->min + (chunk->max - chunk->min) / 2;
-	while (1)
-	{
-		b->p1 = NULL;
-		b->p2 = NULL;
-		b->p3 = NULL;
-		if (ft_lstsize(stacks->stack_a) > 3)
-		{
-			b->p1 = find_biggest(stacks->stack_a);
-			b->p2 = find_second_biggest(stacks->stack_a);
-			b->p3 = find_third_biggest(stacks->stack_a);
-		}
-		target = find_best_target(stacks->stack_a, chunk, b);
-		if (!target)
-			break;
-		move_to_top_a(stacks, target);
-		ft_pb(stacks);
-		if (stacks->stack_b && stacks->stack_b->index < mid)
-			ft_rb(stacks);
-	}
+    mid = chunk->min + (chunk->max - chunk->min) / 2;
+    while (1)
+    {
+        if (ft_lstsize(stacks->stack_a) <= 3)
+            break;
+        b->p1 = find_biggest(stacks->stack_a);
+        b->p2 = find_second_biggest(stacks->stack_a);
+        b->p3 = find_third_biggest(stacks->stack_a);
+        target = find_best_target(stacks->stack_a, chunk, b);
+        if (!target)
+            break;
+        move_to_top_a(stacks, target);
+        ft_pb(stacks);
+        if (stacks->stack_b && stacks->stack_b->index < mid)
+            ft_rb(stacks);
+    }
 }
 
-void	chunk_presort(t_stacks *stacks)
+void    chunk_presort(t_stacks *stacks)
 {
-	t_chunk		chunk;
-	t_biggest	biggest;
+    t_chunk		chunk;
+    t_biggest	biggest;
+	int			strict_max;
 
-	chunk.stack_size = ft_lstsize(stacks->stack_a);
-	chunk.size = choose_chunk_size(chunk.stack_size);
-	chunk.min = 0;
-	chunk.max = chunk.size - 1;
-	while (chunk.stack_size > 3)
-	{
-		build_chunk(stacks, &chunk, &biggest);
-		chunk.min += chunk.size;
-		chunk.max += chunk.size;
-		chunk.stack_size = ft_lstsize(stacks->stack_a);
-	}
+    chunk.size = choose_chunk_size(ft_lstsize(stacks->stack_a));
+    chunk.min = 0;
+    chunk.max = chunk.size - 1;
+    strict_max = ft_lstsize(stacks->stack_a) - 4;
+    while (ft_lstsize(stacks->stack_a) > 3)
+    {
+        if (chunk.max > strict_max)
+            chunk.max = strict_max;
+        build_chunk(stacks, &chunk, &biggest);
+        chunk.min += chunk.size;
+        chunk.max = chunk.min + chunk.size - 1;
+    }
 }
--- srcs/find_target.c
+++ srcs/find_target.c
@@ -6,21 +6,21 @@
 /*   By: spacotto <spacotto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 22:09:12 by spacotto          #+#    #+#             */
-/*   Updated: 2026/01/19 22:52:05 by spacotto         ###   ########.fr       */
+/*   Updated: 2026/01/19 23:37:47 by spacotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	istarget(t_list *node, int min, int max, t_biggest *b)
+static int	istarget(t_list *node, t_chunk *chunk, t_biggest *b)
 {
-	if (node->index < min || node->index > max)
+	if (node->index < chunk->min || node->index > chunk->max)
 		return (0);
-	if (b && b->p1 && node == b->p1)
+	if (b->p1 && node == b->p1)
 		return (0);
-	if (b && b->p2 && node == b->p2)
+	if (b->p2 && node == b->p2)
 		return (0);
-	if (b && b->p3 && node == b->p3)
+	if (b->p3 && node == b->p3)
 		return (0);
 	return (1);
 }
@@ -52,7 +52,7 @@ t_list	*find_best_target(t_list *stack, t_chunk *chunk, t_biggest *b)
 	min_distance = ft_lstsize(stack) + 1;  // Start with impossible value
 	while (current)
 	{
-		if (istarget(current, chunk->min, chunk->max, b))
+		if (istarget(current, chunk, b))
 		{
 			distance = find_distance(stack, current);
 			if (distance < min_distance)
