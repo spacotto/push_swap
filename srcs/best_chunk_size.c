cf5c9cf63646e51f5140d8de24cab4c1d278627d
spacotto <silvia.pacotto@gmail.com>
2026-01-20 10:49:10 +0100

    Update best_chunk_size.c

--- srcs/best_chunk_size.c
+++ srcs/best_chunk_size.c
@@ -11,3 +11,32 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static int	calculate_disorder(t_list *stack)
+{
+	t_list	*current;
+	int		inversions;
+
+	inversions = 0;
+	current = stack;
+	while (current && current->next)
+	{
+		if (current->index > current->next->index)
+			inversions++;
+		current = current->next;
+	}
+	return (inversions);
+}
+
+int	choose_chunk_size(int stack_size)
+{
+	int	chunk_size;
+
+	if (stack_size <= 5)
+		chunk_size = 1;
+	else if (stack_size <= 100)
+		chunk_size = 33;
+	else
+		chunk_size = 66;
+	return (chunk_size);
+}
