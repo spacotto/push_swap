0a54a50bc1878c9a75c7392f22c3db66caafa9af
spacotto <silvia.pacotto@gmail.com>
2026-01-23 20:00:49 +0100

    Update find_biggest.c

--- srcs/find_biggest.c
+++ srcs/find_biggest.c
@@ -12,24 +12,6 @@
 
 #include "push_swap.h"
 
-t_list	*find_biggest(t_list *stack)
-{
-	t_list	*biggest;
-	t_list	*current;
-
-	if (!stack || !stack->next)
-		return (NULL);
-	biggest = stack;
-	current = stack->next;
-	while (current)
-	{
-		if (current->index > biggest->index)
-			biggest = current;
-		current = current->next;
-	}
-	return (biggest);
-}
-
 t_list	*find_second_biggest(t_list *stack)
 {
 	t_list	*biggest;
