8c6ce40d7b722610b8bbd87ec46c43aeae8a824f
spacotto <silvia.pacotto@gmail.com>
2026-01-23 12:38:03 +0100

    Update sort_parsing.c

--- srcs/sort_parsing.c
+++ srcs/sort_parsing.c
@@ -24,7 +24,7 @@ static void	get_tokens(t_stacks *stacks, char *s)
 		number = malloc(1 * sizeof(int));
 		if (!number)
 			return ;
-		*number = ft_atol(token);
+		*number = ft_atoi(token);
 		node = ft_lstnew(number);
 		if (!node)
 		{
