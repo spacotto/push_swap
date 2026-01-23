17fc2befa1d2bedafe999a1134ac40609cbd3920
spacotto <silvia.pacotto@gmail.com>
2026-01-23 22:56:39 +0100

    Update chunk_sort.c

--- srcs/chunk_sort.c
+++ srcs/chunk_sort.c
@@ -12,7 +12,34 @@
 
 #include "push_swap.h"
 
-t_list	*find_biggest(t_list *stack)
+void	sort_three(t_stacks *stacks)
+{
+	int	n1;
+	int	n2;
+	int	n3;
+
+	n1 = *(int *)stacks->stack_a->content;
+	n2 = *(int *)stacks->stack_a->next->content;
+	n3 = *(int *)stacks->stack_a->next->next->content;
+	if (n1 > n2 && n2 < n3 && n1 < n3)
+		ft_sa(stacks);
+	else if (n1 > n2 && n2 > n3)
+	{
+		ft_sa(stacks);
+		ft_rra(stacks);
+	}
+	else if (n1 > n2 && n2 < n3 && n1 > n3)
+		ft_ra(stacks);
+	else if (n1 < n2 && n2 > n3 && n1 < n3)
+	{
+		ft_sa(stacks);
+		ft_ra(stacks);
+	}
+	else if (n1 < n2 && n2 > n3 && n1 > n3)
+		ft_rra(stacks);
+}
+
+static t_list	*find_biggest(t_list *stack)
 {
 	t_list	*biggest;
 	t_list	*current;
