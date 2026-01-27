83fe8de2fa4a4223b2c2e843e1727a57871acf21
spacotto <silvia.pacotto@gmail.com>
2026-01-27 14:05:49 +0100

    Update checker_bonus.c

--- srcs/checker_bonus.c
+++ srcs/checker_bonus.c
@@ -12,33 +12,6 @@
 
 #include "checker.h"
 
-static int	valid_ops(char *token)
-{
-	if (ft_strcmp(token, "sa\n") == 0)
-		return (1);
-	else if (ft_strcmp(token, "sb\n") == 0)
-		return (2);
-	else if (ft_strcmp(token, "ss\n") == 0)
-		return (3);
-	else if (ft_strcmp(token, "pa\n") == 0)
-		return (4);
-	else if (ft_strcmp(token, "pb\n") == 0)
-		return (5);
-	else if (ft_strcmp(token, "ra\n") == 0)
-		return (6);
-	else if (ft_strcmp(token, "rb\n") == 0)
-		return (7);
-	else if (ft_strcmp(token, "rr\n") == 0)
-		return (8);
-	else if (ft_strcmp(token, "rra\n") == 0)
-		return (9);
-	else if (ft_strcmp(token, "rrb\n") == 0)
-		return (10);
-	else if (ft_strcmp(token, "rrr\n") == 0)
-		return (11);
-	return (0);
-}
-
 static void	add_op(t_list **ops, char *op)
 {
 	t_list	*node;
